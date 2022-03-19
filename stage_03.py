87408a77803671af4eb0e094625155cdf05aa398
faisal bin basha <faisalbasha1982gmail.com>
2022-03-19 05:12:53 -0400

    new changes

--- stage_03.py
+++ stage_03.py
@@ -1,11 +1,11 @@
 
-text="file"
+text="added files"
 
 with open("artifacts01.txt","r") as f:
     text = f.read()
 
 with open("artifacts02.txt","w") as f:
-    text = f.write(text = "added lines")
+    f.write(text)
 
 
 print("end of stage02")
